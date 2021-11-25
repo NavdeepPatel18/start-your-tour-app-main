9bd49a159829f32eddd81ccf9e55d677e067420c
navdeeppatel1852 <navdeeppatel.1852@gmail.com>
2021-11-25 12:45:14 +0530

    no change

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPITIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization");
-  // res.setHeader("Access-Control-Allow-Credentials", true);
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
