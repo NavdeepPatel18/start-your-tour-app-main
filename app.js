fa4d27648d299c5c7552caf6f71657bc082b2a0f
navdeeppatel1852 <navdeeppatel.1852@gmail.com>
2021-11-25 12:31:40 +0530

    add hearder

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPITIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-type, Authorization");
+  // res.setHeader("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(isAuth);
+
 app.use(
     "/SYT",
    graphqlHTTP({
@@ -21,7 +34,6 @@ app.use(
     })
   );
 
-  app.use(isAuth);
   mongoose
   .connect(`mongodb+srv://USER:${process.env.MONGO_PASSWORD}@syt.nrodu.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     {
